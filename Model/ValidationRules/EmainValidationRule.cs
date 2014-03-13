f47fdd1e1f29102e3926b5775328b6097f26221e
Sinitsin <ballack.tyta@gmail.com>
2014-03-13 09:13:09 +0300

    Added validation rules

---
+++ Model/ValidationRules/EmainValidationRule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace MVVM_Example.Model.ValidationRules
+{
+    public class EmainValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            String pattern = "@";
+            Regex regex = new Regex(pattern);
+
+            String userInput = (String)value;
+            if (regex.IsMatch(userInput))
+            {
+                return ValidationResult.ValidResult;
+            }
+            else
+            {
+                return new ValidationResult(false, "Email is invalid");
+            }
+        }
+    }
+}
---
+++ Model/ValidationRules/OneWordValidationRule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Text.RegularExpressions;
+
+namespace MVVM_Example.Model.ValidationRules
+{
+    public class OneWordValidationRule : ValidationRule
+    {
+
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            String pattern = "@\\S*";
+            Regex regex = new Regex(pattern);
+            Match match;
+
+            String word = (String)value;
+            if(regex.IsMatch(word))
+                return ValidationResult.ValidResult;
+            else
+                return new ValidationResult(false, "Spaces are restricted");
+        }
+    }
+}
